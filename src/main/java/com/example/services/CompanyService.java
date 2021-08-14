0654e13bf6087f4abbbb9d7be069f3ff19b1d252
stebbinscp <cstebbins@uchicago.edu>
2021-08-14 16:45:17 +0300

    Added company service

---
+++ src/main/java/com/example/services/CompanyService.java
@@ -0,0 +1,24 @@
+package com.example.services;
+
+import com.example.models.Company;
+
+import java.util.List;
+
+public class CompanyService {
+
+    public List<Company> findAll(){
+        //use repo to call findall
+        return null;
+    }
+
+    public List<Company> add(Company company){
+        //use repo to call add
+        return null;
+    }
+
+    public Company get(String number) {
+        // use repo to get number
+        return null;
+    }
+
+}
